8310c981dc8f62be9d90ca8c4864c726a74ea632
alex853 <alexxxey.kornev@gmail.com>
2024-11-07 20:41:07 +0200

    Hours extended

--- src/main/java/net/simforge/fseconomy/tools/app/ScheduledTasks.java
+++ src/main/java/net/simforge/fseconomy/tools/app/ScheduledTasks.java
@@ -21,7 +21,7 @@ public class ScheduledTasks {
                     "Cessna Citation X"),
             new VIPAssignmentsByMakeModel("Cessna Citation CJ4 (MSFS)", 10, "C25C"),
             new VIPAssignmentsByMakeModel("Cessna Citation Longitude", 8, "C700"),
-            new PlanAircraftItinerary("G-SDRY", 10, 4, 10)
+            new PlanAircraftItinerary("G-SDRY", 10, 4, 14)
     );
 
     @Scheduled(fixedRate = 60000)
