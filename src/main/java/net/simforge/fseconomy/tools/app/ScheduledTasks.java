4e8e333a113155b6bbb8cc7e789fad050d07b7ea
alex853 <alexxxey.kornev@gmail.com>
2025-01-14 12:05:08 +0200

    Added C404 and C310 tasks tracking

--- src/main/java/net/simforge/fseconomy/tools/app/ScheduledTasks.java
+++ src/main/java/net/simforge/fseconomy/tools/app/ScheduledTasks.java
@@ -21,6 +21,8 @@ public class ScheduledTasks {
                     "Cessna Citation X"),
             new VIPAssignmentsByMakeModel("Cessna Citation CJ4 (MSFS)", 10, "C25C"),
             new VIPAssignmentsByMakeModel("Cessna Citation Longitude", 8, "C700"),
+            new VIPAssignmentsByMakeModel("Cessna 404 Titan", 9, "C404"),
+            new VIPAssignmentsByMakeModel("Cessna 310", 5, "C310"),
             new VIPAssignmentsInEurope(10),
             new PlanAircraftItinerary("G-SDRY", 10, 4, 14)
     );
